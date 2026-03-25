71d5a6fd01051cdaf84fd3341f009ad43bd52eed
garder500 <jeremy27.clara22@gmail.com>
2026-03-25 18:15:21 +0100

    test: ajouter des tests pour les paramètres d'édition des messages et des interactions

---
+++ packages/app/test/action_parameter_definitions_test.dart
@@ -0,0 +1,32 @@
+import 'package:bot_creator/routes/app/builder/action_type_extension.dart';
+import 'package:bot_creator/routes/app/builder/action_types.dart';
+import 'package:bot_creator/types/action.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+ParameterDefinition _findByKey(List<ParameterDefinition> defs, String key) {
+  return defs.firstWhere(
+    (def) => def.key == key,
+    orElse: () => throw StateError('Missing parameter definition: $key'),
+  );
+}
+
+void main() {
+  group('action parameter definitions', () {
+    test('editMessage exposes editable embeds and clearEmbeds', () {
+      final defs = BotCreatorActionType.editMessage.parameterDefinitions;
+
+      expect(_findByKey(defs, 'embeds').type, ParameterType.embeds);
+      expect(_findByKey(defs, 'clearEmbeds').type, ParameterType.boolean);
+      expect(_findByKey(defs, 'clearEmbeds').defaultValue, isFalse);
+    });
+
+    test('editInteractionMessage exposes editable embeds and clearEmbeds', () {
+      final defs =
+          BotCreatorActionType.editInteractionMessage.parameterDefinitions;
+
+      expect(_findByKey(defs, 'embeds').type, ParameterType.embeds);
+      expect(_findByKey(defs, 'clearEmbeds').type, ParameterType.boolean);
+      expect(_findByKey(defs, 'clearEmbeds').defaultValue, isFalse);
+    });
+  });
+}
